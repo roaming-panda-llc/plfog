5c10957a686809b5e53f6e7969bdc55e5d547108
Josh Plaza <plazajosue2@gmail.com>
2026-04-16 11:09:58 -0700

    test(membership): remove duplicate can_edit_guild test now that FK is gone

--- tests/membership/models_spec.py
+++ tests/membership/models_spec.py
@@ -802,12 +802,6 @@ def describe_member_fog_roles():
             guild.guild_leads.add(other_lead)
             assert member.can_edit_guild(guild) is False
 
-        def it_is_true_for_guild_lead_assigned_via_m2m():
-            lead = MemberFactory(fog_role=Member.FogRole.MEMBER)
-            guild = GuildFactory()
-            guild.guild_leads.add(lead)
-            assert lead.can_edit_guild(guild) is True
-
         def it_is_false_when_member_not_in_guild_leads():
             member = MemberFactory(fog_role=Member.FogRole.MEMBER)
             guild = GuildFactory()
